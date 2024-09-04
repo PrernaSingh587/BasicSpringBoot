dab1f36933afe0ab74711224c361fa240ce8dba6
Prerna Singh <prernasingh@Prernas-MacBook-Air.local>
2024-09-04 21:55:48 +0530

    first commit

---
+++ main/java/com/example/demo/BasicSpringbootAppForPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringbootAppForPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringbootAppForPracticeApplication.class, args);
+	}
+
+}
